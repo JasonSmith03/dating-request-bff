ff297fa6e669990d440c89f9537885258209073f
JasonSmith03 <39653254+JasonSmith03@users.noreply.github.com>
2026-07-17 16:10:39 -0400

    changed service request

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
-FROM eclipse-temurin:17-jdk
+# ---------- Build Stage ----------
+FROM eclipse-temurin:21-jdk
 
 WORKDIR /app
 
-COPY build/libs/*.jar app.jar
+COPY . .
 
-ENTRYPOINT [
-"java",
-"-jar",
-"app.jar"
-]
+RUN chmod +x gradlew
+
+RUN ./gradlew clean bootJar --no-daemon
+
+# ---------- Runtime Stage ----------
+FROM eclipse-temurin:21-jre
+
+WORKDIR /app
+
+COPY --from=0 /app/build/libs/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/date_planner_bff/operation/DateRequestService.java
+++ src/main/java/com/example/date_planner_bff/operation/DateRequestService.java
@@ -1,13 +1,14 @@
 package com.example.date_planner_bff.operation;
 
 import com.example.date_planner_bff.model.DateRequestDto;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
 @Service
+@Slf4j
 public class DateRequestService {
 
 
@@ -27,18 +28,12 @@ public class DateRequestService {
                         "time", request.time()
                 );
 
-        supabaseClient.get()
-                .uri("/auth/v1/user")
-                .retrieve()
-                .bodyToMono(Void.class);
-
         supabaseClient.post()
                 .uri("/rest/v1/date_requests")
                 .bodyValue(body)
                 .retrieve()
                 .bodyToMono(Void.class)
                 .block();
-
+        log.info("Date request saved successfully: name: {}", request.name());
     }
-
 }
